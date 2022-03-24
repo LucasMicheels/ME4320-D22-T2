f0653bec9fec44db0802b5d3dde4485528f413a9
bsang857 <brendyn857@gmail.com>
2022-03-24 16:45:22 -0400

    Made setter and rope class

---
+++ src/rope.m
@@ -0,0 +1,18 @@
+classdef rope
+    
+    properties
+        x = 0;
+        y = 0;
+        hoistRope = true;
+    end
+    
+    methods
+        function set.x(obj,x)
+            obj.x = x
+        end
+        
+        
+  
+    end
+end
+
