8ed442ae3d5540d0ce76f3f055d222e683387a13
bsang857 <74837631+bsang857@users.noreply.github.com>
2022-03-31 01:41:27 -0400

    just some cleaning; getting rid of unnecessary files in this branch

--- src/frame.m
+++ src/frame.m
@@ -53,76 +53,6 @@ classdef Frame < handle
             
             clear data timeLocation amplitudeLocation distanceLocation angleLocation; 
             clear columnNames time distance angle amplitude;
-        end
-        
-        % Assuming format of column vectors of [time, angle, distance, amplitude]
-        % Will take in filtered data (so only the ropes) and merge data points that
-        % are close together; will need to do some brainstorming for edge cases;
-        % maybe instead of just looking at the mean, we can make an imaginary
-        % circle and whatever is an outlier is considered another rope; final
-        % output is a nx2 matrix with angles and dist; assuming polar coordinates
-        function ropes = mergeDataPoints(obj, filteredData)
-            ropes = [];
-            cluster = [filteredData(1,:)];
-            [rows, ~] = size(filteredData);
-            for i = 2:rows
-                if and(and(...
-                        filteredData(i, 2) <= filteredData(i - 1, 2) + 0.005,...
-                        filteredData(i, 2) >= filteredData(i - 1, 2) - 0.005),and(...
-                        filteredData(i, 3) <= filteredData(i - 1, 3) + 3,...
-                        filteredData(i, 3) >= filteredData(i - 1, 3) - 3))
-                    cluster = [cluster; filteredData(i,:)];
-                else
-                    ropes = [ropes; mean(cluster, 2) mean(cluster, 3)];
-                    cluster = [];
-                end
-        
-            end
-		end
-
-		% filters walls based on dimensions; don't understand why it is
-		% coded the way it is
-		function filteredData = wallFiltering(obj, rawData)
-			[rows, ~] = size(filteredData);
-			for i = 1:rows
-    			if (rawData(i, 2) > 180) | (rawData(i, 2) < 0)
-        			if i == size(rawData,2)-1
-            			angle = rawData(1:i-1, 2);
-						distance = distance(1:i-1, 3);
-            			amplitude = rawData(1:i-1, 4);
-            			data = rawData(1:i-1, 1);
-            			time = rawData(1:i-1, 1);
-        			else
-            			angle = cat(1, rawData(1:i-1, 2),angle(i+1:size(angle,1), 1));
-            			amplitude = cat(1, amplitude(1:i-1, 1),amplitude(i+1:size(amplitude,1), 1));
-            			data = cat(1, data(1:i-1, 1),data(i+1:size(data,1), 1));
-            			distance = cat(1, distance(1:i-1, 1),distance(i+1:size(distance,1), 1));
-            			time = cat(1, time(1:i-1, 1),time(i+1:size(time,1), 1));
-        			end
-        			i = i-1;
-    			else
-        			x = distance(i)*cos(angle(i));
-        			y = distance(i)*sin(angle(i));
-        			if (x > 0.98 & x < 1.02) | (y > 1.98 & y < 2.02)
-            			if i == size(angle,1)-1
-                			angle = angle(1:i-1, 1);
-                			amplitude = amplitude(1:i-1, 1);
-                			data = data(1:i-1, 1);
-                			distance = distance(1:i-1, 1);
-                			time = time(1:i-1, 1);
-            			else
-                			angle = cat(1, angle(1:i-1, 1),angle(i+1:size(angle,1), 1));
-                			amplitude = cat(1, amplitude(1:i-1, 1),amplitude(i+1:size(amplitude,1), 1));
-                			data = cat(1, data(1:i-1, 1),data(i+1:size(data,1), 1));
-                			distance = cat(1, distance(1:i-1, 1),distance(i+1:size(distance,1), 1));
-                			time = cat(1, time(1:i-1, 1),time(i+1:size(time,1), 1));
-            			end
-        			i = i-1;
-        			end
-    			end
-			
-    			i = i + 1;
-			end
 		end
 
 		% Brendyn's code to filter walls based on dimensions; RAWMEAT is
@@ -163,8 +93,6 @@ classdef Frame < handle
 			end
 			hold off
 		end
-
-	 	
 
 	end
 end
