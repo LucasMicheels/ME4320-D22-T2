74339284ddafb388e1cc5f71580538147b6cfa47
bsang857 <74837631+bsang857@users.noreply.github.com>
2022-03-31 22:54:18 -0400

    added ability to plot separate figures

--- src/main.m
+++ src/main.m
@@ -7,6 +7,9 @@ frame = Frame;
 
 % Main script
 its_RAW = frame.loadData('360 2 static ropes 87cm x 85cm 23.75cm from the back and 6cm from the left side.xlsx');
-BS = frame.wallFilteringDIMENSIONS(its_RAW, 10000, 10000, 60, 60, false);
-%singular = frame.mergeDataPoints(BS);
+BS = frame.wallFilteringDIMENSIONS(its_RAW, 870, 850, 60, 238, false);
+figure(1)
 frame.justPlotPls(BS)
+singular = frame.mergeDataPoints(BS);
+figure(2)
+frame.justPlotPls(singular)
