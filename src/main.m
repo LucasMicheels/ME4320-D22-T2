7d52505141bb9ab88200a9adb83d39a39906676e
bsang857 <74837631+bsang857@users.noreply.github.com>
2022-04-01 01:44:50 -0400

    as of now it isn't filtering anything. May need to look into changing the data to process less

--- src/main.m
+++ src/main.m
@@ -1,15 +1,22 @@
 clc;
 clear;
 
+
+% Setup user variables
+sensorPosX = 60;
+sensorPosY = 238;
+elevatorDimensionsX = 870;
+elevatorDimensionsY = 850;
+
 % Setup classes
 frame = Frame;
-
+frame.setPosition(sensorPosX, sensorPosY, elevatorDimensionsX, elevatorDimensionsY);
 
 % Main script
 its_RAW = frame.loadData('360 2 static ropes 87cm x 85cm 23.75cm from the back and 6cm from the left side.xlsx');
-BS = frame.wallFilteringDIMENSIONS(its_RAW, 870, 850, 60, 238, false);
-figure(1)
-frame.justPlotPls(BS)
-singular = frame.mergeDataPoints(BS);
-figure(2)
-frame.justPlotPls(singular)
+BS = frame.wallFilteringDIMENSIONS(its_RAW, false);
+% figure(1)
+%frame.justPlotPls(BS)
+% singular = frame.mergeDataPoints(BS);
+% figure(2)
+% frame.justPlotPls(singular)
