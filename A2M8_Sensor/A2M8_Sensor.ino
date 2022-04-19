994529fff4df01ba0d2b23ad72d85a28a7c05ba8
anichols3 <anichols3@wpi.edu>
2022-04-19 16:54:36 -0400

    Sensor Readings

--- A2M8_Sensor/A2M8_Sensor.ino
+++ A2M8_Sensor/A2M8_Sensor.ino
@@ -1,7 +1,7 @@
 
 
 #include <SoftwareSerial.h> //header file of software serial port 
-SoftwareSerial Serial1(2,3); //define software serial port name as Serial1 and define pin2 as RX and pin3 as TX 
+SoftwareSerial Serial1(3,2); //define software serial port name as Serial1 and define pin2 as RX and pin3 as TX 
 
 //SoftwareSerial Serial2(4,5); 
 /* For Arduinoboards with multiple serial ports like DUEboard, interpret above two pieces of code and 
